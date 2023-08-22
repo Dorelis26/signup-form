33bdf862dfce6dc47ece6d29eae2b138b394b59a
Nicolas Dorelis <115403319+Dorelis26@users.noreply.github.com>
2023-08-22 02:04:39 -0300

    Add files via upload

---
+++ sign up form/script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container");
+const signupbtn = document.querySelector(".green-bg button");
+signupbtn.addEventListener("click", ()=>{
+    container.classList.toggle("change");
+})
